2d2cce96f54ac229c29e6dcfe446ae90ebc0c193
Avenez <145536778+Avenez@users.noreply.github.com>
2023-11-08 16:55:43 +0100

    Add files via upload

---
+++ 3_WEEK/3_DAY/script/script.js
@@ -0,0 +1,36 @@
+let bottone = document.getElementById("aggiungi");
+bottone.addEventListener('click', addElement);
+
+
+
+function addElement(){
+    let ul = document.querySelector("ul");
+    let elementLista = document.getElementById("elementoLista").value;
+    if(elementLista !== ""){
+    let li = document.createElement("li");
+    li.innerHTML = `${elementLista}`
+    ul.appendChild(li);
+    let idexId = document.querySelectorAll("li")
+    for(i = 0 ; i < idexId.length; i++){
+        li.removeAttribute("id");
+        li.setAttribute("id",`${i}`)
+        li.innerHTML = `<img onclick="elimina(${i})" src="assets/trash-outline.svg" alt="cestino"> ${elementLista}`
+    }
+}
+else{
+    alert("Devi inserire un task");
+}
+elementLista.value = '';
+}
+
+var lista = document.querySelector('ul');
+    lista.addEventListener('click', function(x) {
+    if (x.target.tagName === 'LI') {
+        x.target.classList.toggle('line');
+    }
+    }, false);
+
+function elimina(x){
+    document.getElementById(x).remove();
+}
+
